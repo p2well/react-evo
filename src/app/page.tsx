9cf5333f501f007a377e38e464d94836f0099477
p2well <p2well@outlook.com>
2025-01-23 15:38:40 +0100

    Improve page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,37 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+    <div className="container mx-auto min-h-screen flex flex-col">
+      <div role="region" aria-label="skip navigation">
+        <a
+          href="#main"
+          className="sr-only focus:not-sr-only focus:absolute focus:top-0 focus:left-0 focus:bg-black focus:text-white focus:dark:bg-white focus:dark:text-black focus:border-4 focus:border-primary-500 focus:p-1"
+        >
+          Skip to main content
+        </a>
+      </div>
+      <header
+        id="header"
+        className="flex flex-row flex-wrap sm:flex-nowrap items-center"
+      >
+        <div className="font-medium text-lg text-nowrap pr-4">
+          React Evolution
+        </div>
+      </header>
+      <main id="main" className="container flex flex-grow justify-center">
+        <div className="flex justify-between">
+          <Image
+            className="dark:invert"
+            src="/next.svg"
+            alt="Next.js logo"
+            width={180}
+            height={38}
+            priority
+          />
+        </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer id="footer" className="text-center py-4">
         <p>&copy; 2025 Your Company. All rights reserved.</p>
       </footer>
     </div>
