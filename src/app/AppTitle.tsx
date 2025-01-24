0f97682f533f1859e0055d31e0db7a0994a28305
p2well <p2well@outlook.com>
2025-01-24 16:31:49 +0100

    Increase the text size for improved visibility

--- src/app/AppTitle.tsx
+++ src/app/AppTitle.tsx
@@ -6,7 +6,7 @@ interface AppTitleProps {
 
 const AppTitle: React.FC<AppTitleProps> = ({ title }) => {
   return (
-    <div className="font-medium text-lg text-nowrap px-4 py-2">{title}</div>
+    <div className="font-medium text-xl text-nowrap px-4 py-2">{title}</div>
   );
 };
 
