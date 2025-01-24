46439a57e10eff55eb5434cf6073463fc7587d6d
p2well <p2well@outlook.com>
2025-01-24 16:31:08 +0100

    Add top border to improve look and feel

--- src/app/AppFooter.tsx
+++ src/app/AppFooter.tsx
@@ -1,6 +1,6 @@
 export default function AppFooter() {
   return (
-    <footer id="footer" className="text-center py-4">
+    <footer id="footer" className="text-center py-4 dark:border-t dark:border-primary-950">
       <p>&copy; 2025 Your Company. All rights reserved.</p>
     </footer>
   );
