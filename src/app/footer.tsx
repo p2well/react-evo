b6e2b34a07b0661a5532e6062820bfed9b346618
p2well <p2well@outlook.com>
2025-01-24 13:09:37 +0100

    Refactor header and footer as components

---
+++ src/app/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer id="footer" className="text-center py-4">
+      <p>&copy; 2025 Your Company. All rights reserved.</p>
+    </footer>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,24 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+
+import Footer from './footer';
+import Header from './header';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "React Evolution",
-  description: "A cutting-edge, Next.js-powered web app template designed for rapid development and seamless performance.",
+  title: 'React Evolution',
+  description:
+    'A cutting-edge, Next.js-powered web app template designed for rapid development and seamless performance.',
 };
 
 export default function RootLayout({
@@ -27,7 +31,21 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="container mx-auto min-h-screen flex flex-col">
+          <div role="region" aria-label="skip navigation">
+            <a
+              href="#main"
+              className="sr-only focus:not-sr-only focus:absolute focus:top-0 focus:left-0 focus:bg-black focus:text-white focus:dark:bg-white focus:dark:text-black focus:border-4 focus:border-primary-500 focus:p-1"
+            >
+              Skip to main content
+            </a>
+          </div>
+          <Header />
+          <main id="main" className="container flex flex-grow justify-center">
+            {children}
+          </main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
